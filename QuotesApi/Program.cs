0803f49648a310af9ec2c343dd298c07e75d1ffd
Vashisht2002 <ssssumit120@gmail.com>
2026-08-10 15:14:07 +0530

    Complete Day 1 Quotes API

--- QuotesApi/Program.cs
+++ QuotesApi/Program.cs
@@ -1,8 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using QuotesApi.Data;
 using QuotesApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddProblemDetails();
+
+builder.Services.AddExceptionHandler(options =>
+{
+    options.ExceptionHandler = async context =>
+    {
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "An unexpected error occurred.",
+            Detail = "The server encountered an unexpected error."
+        };
+
+        context.Response.StatusCode =
+            StatusCodes.Status500InternalServerError;
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    };
+});
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
