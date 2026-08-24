27ba0cd366a24acb29083aaa480057ca131e1d58
Vashisht2002 <ssssumit120@gmail.com>
2026-08-24 12:04:30 +0530

    Complete Day 13 Angular list and detail

--- Day-13/quotes-angular/src/app/app.ts
+++ Day-13/quotes-angular/src/app/app.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Quotes } from './quotes/quotes';
+import { QuotesList } from './quotes-list/quotes-list';
 
 @Component({
   selector: 'app-root',
-  imports: [Quotes],
+  imports: [Quotes, QuotesList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  readonly activeTab = signal<'browse' | 'explorer'>('browse');
+
+  setTab(tab: 'browse' | 'explorer'): void {
+    this.activeTab.set(tab);
+  }
+}
